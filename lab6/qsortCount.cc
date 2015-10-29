c6097a5162b74607486c9a07890f5fd038342070
Steven Wallace <x9u9a@ugrad.cs.ubc.ca>
2015-10-29 11:05:48 -0700

    part 3 and 4 done

--- lab6/qsortCount.cc
+++ lab6/qsortCount.cc
@@ -3,57 +3,71 @@
 #include <cstdlib>
 
 int * x;
-int comps= 0;
+int comps = 0;
 
 void swap(int & a, int & b) {
-	int tmp = a;
-	a = b;
-	b = tmp;
+    int tmp = a;
+    a = b;
+    b = tmp;
 }
 
 int randint(int a, int b) {
-	return a + (rand() % (b - a + 1));
+    return a + (rand() % (b - a + 1));
 }
 
 void quicksort(int a, int b) {
-	if (a >= b) return;
-	int p = randint(a,b); // pivot
-	swap(x[a], x[p]);
-	int m = a;
-	int i;
-	// in-place partition:
-	for (i = a+1; i <= b; i++) {
-	  comps++;
-		if (x[i] < x[a])
-			swap(x[++m], x[i]);
-	}
-	swap(x[a],x[m]);
-	quicksort(a, m-1);
-	quicksort(m+1, b);
+    if (a >= b) return;
+    int p = randint(a,b); // pivot
+    swap(x[a], x[p]);
+    int m = a;
+    int i;
+    // in-place partition:
+    for (i = a+1; i <= b; i++) {
+        comps++;
+        if (x[i] < x[a])
+            swap(x[++m], x[i]);
+    }
+    swap(x[a],x[m]);
+    quicksort(a, m-1);
+    quicksort(m+1, b);
 }
 
-#define NN 1000
+int qc(int n) {
+    if (n <= 1) return 0;
+    int p = randint(0,n-1);
+    return n-1 + qc(p) + qc(n-p-1);
+}
+
+float c(int n) {
+    if (n <=1) return 0;
+    float sum = 0.0;
+    for (int m=1; m <= n; m++) 
+        sum += n-1 + c(m-1) + c(n-m);
+    return sum/n;
+}
+
+#define NN 10
 
 int main(int argc, char *argv[]) {
-	srand(time(0));
-	int sum = 0;
-	// change the following code
-	for ( int j = 0; j < 100; j++ ){
-	  x = new int[NN];
-	  for (int i=0; i<NN; ++i) {
-	    x[i] = rand() % NN;
-	  }
-	
-	  quicksort(0, NN-1);
-	  for (int i=0; i<NN; ++i) {
-	    //	std::cout << x[i] << " ";
-	  }
-	  // std::cout << std::endl;
-	  //std::cout << "Number of comparisons are " << comps << " , Size of array is "<< NN << std::endl;
-	  sum += comps;
-	  delete[] x;
-	}
-	std::cout << "Average number of comparisons are "<< sum/100 << std::endl;
-	return 0;
+    srand(time(0));
+    // change the following code
+    x = new int[NN];
+    for (int j = 0; j < 100; j++) {
+        for (int i=0; i<NN; ++i) {
+            x[i] = rand() % NN;
+        }
+
+        quicksort(0, NN-1);
+        for (int i=0; i<NN; ++i) {
+            //std::cout << x[i] << " ";
+        }
+        //std::cout << std::endl;
+    }
+    delete[] x;
+    std::cout << "Average number of comparisons: " << comps/100 << std::endl;
+    std::cout << "Estiamted number of comparisons: " << qc(NN) << std::endl;
+    if (NN <= 20)
+        std::cout << "Average estimated number of comparisons: " << c(NN) << std::endl;
+    return 0;
 }
 
