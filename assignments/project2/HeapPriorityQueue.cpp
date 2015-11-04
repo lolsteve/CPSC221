2f5e9f34a3d96edd6bab778ba12c2bd48ea30e88
Steven Wallace <x9u9a@ugrad.cs.ubc.ca>
2015-11-04 10:25:21 -0800

    project2 initial commit

---
+++ assignments/project2/HeapPriorityQueue.cpp
@@ -0,0 +1,54 @@
+//HeapPriorityQueue.cpp
+#ifndef _HEAPPRIORITYQUEUE_CPP
+#define _HEAPPRIORITYQUEUE_CPP
+
+#include "HeapPriorityQueue.hpp"
+#include <cassert>
+#include <cstdlib>//for NULL
+#include <iostream>
+
+HeapPriorityQueue::HeapPriorityQueue() {
+  // Empty... nothing needs to be done.
+}
+
+HeapPriorityQueue::~HeapPriorityQueue() {
+  // no clean-up to do, since the heap is not dynamically allocated
+}
+
+void HeapPriorityQueue::put_in(PuzzleState *elem) {
+  // TODO:  Put your code here!
+}
+
+PuzzleState * HeapPriorityQueue::take_out() {
+  assert(!is_empty());
+
+  // TODO:  Put your code here!
+}
+
+
+bool HeapPriorityQueue::is_empty() {
+  return (heap.size() == 0);
+}
+
+int HeapPriorityQueue::parent(int index) {
+  return (index - 1) / 2;
+}
+
+int HeapPriorityQueue::first_child(int index) {
+  return 2 * index + 1;
+}
+
+bool HeapPriorityQueue::is_root(int index) {
+  return index == 0;
+}
+
+bool HeapPriorityQueue::is_leaf(int index) {
+  return num_children(index) == 0;
+}
+
+int HeapPriorityQueue::num_children(int index) {
+  int fchild = first_child(index);
+  return max(0, min(2, (int)heap.size() - fchild));
+}
+
+#endif
